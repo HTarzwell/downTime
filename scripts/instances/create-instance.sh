80df2b1eef3913d061316da36ba5bc7e38af8a02
HTarzwell <htarzwell@gmail.com>
2018-03-08 10:52:37 -0500

    Edited curl script in create instance
    -Added downtime_instance to line 4
    
    -Added new inputs to lines 11-14

--- scripts/instances/create-instance.sh
+++ scripts/instances/create-instance.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/doctors"
+URL_PATH="/downtime_instances"
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
-    "doctor": {
-      "given_name": "'"${GIVEN_NAME}"'"
-      "family_name": "'"${FAMILY_NAME}"'"
-      "specialty": "'"${SPECIALTY}"'"
+    "downtime_instance": {
+      "date": "'"${DATE}"'"
+      "time": "'"${TIME}"'"
+      "activity": "'"${ACTIVITY}"'"
     }
   }'
 
