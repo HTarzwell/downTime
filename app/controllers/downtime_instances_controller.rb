0f6b7a332b27a73c1dfd25d2c1af040f6b9b7c0f
HTarzwell <htarzwell@gmail.com>
2018-03-12 10:57:07 -0400

    Required params for create instance, WORKS

--- app/controllers/downtime_instances_controller.rb
+++ app/controllers/downtime_instances_controller.rb
@@ -17,7 +17,7 @@ class DowntimeInstancesController < OpenReadController
   def create
     @downtime_instance = current_user.downtime_instances.build(downtime_instance_params)
 
-    if @downtime_instance.save
+    if @downtime_instance.save && !downtime_instance_params.blank?
       render json: @downtime_instance, status: :created, location: @downtime_instance
     else
       render json: @downtime_instance.errors, status: :unprocessable_entity
@@ -26,7 +26,7 @@ class DowntimeInstancesController < OpenReadController
 
   # PATCH/PUT /downtime_instances/1
   def update
-    if @downtime_instance.update(downtime_instance_params)
+    if @downtime_instance.update(downtime_instance_params) && !downtime_instance_params.blank?
       render json: @downtime_instance
     else
       render json: @downtime_instance.errors, status: :unprocessable_entity
@@ -46,6 +46,6 @@ class DowntimeInstancesController < OpenReadController
 
     # Only allow a trusted parameter "white list" through.
     def downtime_instance_params
-      params.require(:downtime_instance, :date, :time, :activity).permit(:user_id)
+      params.require(:downtime_instance).permit(:user_id, :date, :time, :activity)
     end
 end
